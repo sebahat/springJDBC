d5de860ab7e29b805664927a13a4ba7b5e574290
sebahat <sebahat.tnc@gmail.com>
2019-05-15 16:13:35 +0300

    Add files via upload
    emptyproject

---
+++ jdbcDto/src/main/java/com/JdbcDtoPattern/jdbcDto/JdbcDtoApplication.java
@@ -0,0 +1,13 @@
+package com.JdbcDtoPattern.jdbcDto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcDtoApplication.class, args);
+	}
+
+}
